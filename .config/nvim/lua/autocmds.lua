6a25e05b673e2e4f77a7e2872904a17a617a208e
wat3rm3ll0n <29581262+wat3rm3ll0n@users.noreply.github.com>
2024-07-15 08:42:04 -0700

    feat: clean on save + obsidian options
    Adds two new autocommands based around cleaning up files on save
    
    1. Remove trailing whitespace from lines before saving file
    2. Call Black autoformatter on python files after save and refresh the
    file in editor window
    
    Additionally, adds some options for obsidian markdown editing. This is
    in prep for creating a shortcut for creating new notes to set up a quick
    note system

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,10 +1,31 @@
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = highlight_group,
+  pattern = '*',
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
-  pattern = "*",
+})
+
+-- [[ Clean on save ]]
+-- remove trailing whitespace from all lines before saving a file)
+local clean_on_save = vim.api.nvim_create_augroup('clean_on_save', {})
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  group = clean_on_save,
+  pattern = '*',
+  command = [[%s/\s\+$//e]],
+})
+
+-- [[ Format on save]]
+-- after file is saved, format file then reload the file
+local black = vim.api.nvim_create_augroup('black', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+  group = black,
+  pattern = '*.py',
+  callback = function()
+    vim.cmd 'silent !black --quiet %'
+    vim.cmd 'edit'
+  end,
 })
--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -28,6 +28,12 @@ return {
           path = '~/notes/work',
         },
       },
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+      },
+      notes_subdir = 'inbox',
+      new_notes_location = 'notes_subdir',
     },
   },
 }
