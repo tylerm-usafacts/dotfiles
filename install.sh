7fdd3e221070bdf7fb3ce28ae7886b2fbf3fcac8
tylerm <tylerm@usafacts.org>
2025-07-10 16:28:49 -0700

    chore: further devpod revisions

--- install.sh
+++ install.sh
@@ -3,14 +3,19 @@
 echo "Installing dotfiles config..."
 apt update -y && apt install -y build-essential fzf gcc gh lazygit lua-language-server make neovim node pyenv-virtualenv ripgrep starship tree unzip xclip zoxide
 
+echo "Creating config directories..."
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/starship
+mkdir -p ~/.config/wezterm
+
 echo "Copying in nvim..."
-cp -r ~/dotfiles/.config/nvim/ ~/.config/nvim/
+cp -r ~/dotfiles/.config/nvim/* ~/.config/nvim/
 
 echo "Copying in starship..."
-cp -r ~/dotfiles/.config/starship/ ~/.config/starship/
+cp -r ~/dotfiles/.config/starship/* ~/.config/starship/
 
 echo "Copying in wezterm..."
-cp -r ~/dotfiles/.config/wezterm/ ~/.config/wezterm/
+cp -r ~/dotfiles/.config/wezterm/* ~/.config/wezterm/
 
 echo "Copying in .zshrc..."
-cp ~/dotfiles/.zshrc ~.zshrc
+cp ~/dotfiles/.zshrc ~/.zshrc
