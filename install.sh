ad451e32683b51d612035be06fb3d41318f1635c
tylerm <tylerm@usafacts.org>
2025-07-11 08:59:01 -0700

    chore: use pre-built installers for neovim + starship

--- install.sh
+++ install.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
+# Install necessary dependencies
 echo "Installing dotfiles config..."
-sudo apt update -y && sudo apt install -y build-essential fzf gcc gh lazygit lua-language-server make neovim node pyenv-virtualenv ripgrep starship tree unzip xclip zoxide
 
+echo "Installing dependencies..."
+sudo apt update -y && sudo apt install -y zsh build-essential fzf gcc gh lazygit lua-language-server make node pyenv-virtualenv ripgrep tree unzip xclip zoxide
+
+# Set default shell to zsh
+echo "Switch default shell to zsh..."
+usermod -s $(which zsh) $(whoami)
+
+echo "Installing neovim..."
+# Install pre-built neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+export PATH="$PATH:/opt/nvim-linux-x86_64/bin" > ~/.zshrc
+
+echo "Installing starship..."
+# Install starship from installer
+curl -sS https://starship.rs/install.sh | sh
+
+# Create config directories
 echo "Creating config directories..."
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/starship
 mkdir -p ~/.config/wezterm
 
-echo "Copying in nvim..."
+echo "Copying in configs..."
 cp -r ~/dotfiles/.config/nvim/* ~/.config/nvim/
-
-echo "Copying in starship..."
 cp -r ~/dotfiles/.config/starship/* ~/.config/starship/
-
-echo "Copying in wezterm..."
 cp -r ~/dotfiles/.config/wezterm/* ~/.config/wezterm/
-
-echo "Copying in .zshrc..."
 cp ~/dotfiles/.zshrc ~/.zshrc
+
