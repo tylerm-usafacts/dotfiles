54c695bc2283e256d80304430f60caf99f8b523a
tylerm <tylerm@usafacts.org>
2025-07-11 15:45:15 -0700

    chore: continued devpod support testing

--- install.sh
+++ install.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
 # Install necessary dependencies
-echo "Installing dotfiles config..."
+echo "Upgrading ubuntu..."
+sudo apt update -y && sudo apt install -y
+sudo apt install update-manager-core -y
+sudo do-release-upgrade -f DistUpgradeViewNonInteractive
 
-echo "Installing dependencies..."
-sudo apt update -y && sudo apt install -y zsh build-essential fzf gcc gh lazygit lua-language-server make node pyenv-virtualenv ripgrep tree unzip xclip zoxide
-
-# Set default shell to zsh
-echo "Switch default shell to zsh..."
-usermod -s $(which zsh) $(whoami)
-
-echo "Installing neovim..."
-# Install pre-built neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin' >> ~/.zshrc
-
-echo "Installing starship..."
-# Install starship from installer
-curl -sS https://starship.rs/install.sh | sh -y
+echo "Install and switch default shell to zsh..."
+sudo apt update -y && sudo apt install -y zsh
+sudo usermod -s $(which zsh) $(whoami)
 
 # Create config directories
 echo "Creating config directories..."
@@ -33,3 +22,29 @@ cp -r ~/dotfiles/.config/starship/* ~/.config/starship/
 cp -r ~/dotfiles/.config/wezterm/* ~/.config/wezterm/
 cp ~/dotfiles/.zshrc ~/.zshrc
 
+echo "Installing nvm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | sh
+
+echo "Installing dependencies..."
+sudo apt update -y && sudo apt install -y build-essential fzf gcc gh make ripgrep tree unzip xclip zoxide
+
+# Install pyenv
+echo "Installing pyenv..."
+curl -fsSL https://pyenv.run | sh
+
+echo "Installing lazygit..."
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
+echo "Installing neovim..."
+# Install pre-built neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.zshrc
+
+echo "Installing starship..."
+# Install starship from installer
+curl -sS https://starship.rs/install.sh | sh -s -- --yes
