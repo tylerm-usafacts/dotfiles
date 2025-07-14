a6b7664a5cc98284e04a6a8544e4340f89fac6e1
tylerm <tylerm@usafacts.org>
2025-07-14 10:17:29 -0700

    chore: cleanup on install.sh + try to install latest node

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Install necessary dependencies
+# Install necessary dependencies for lts distro upgrade
 echo "Upgrading ubuntu..."
 sudo apt update -y && sudo apt install -y
 sudo apt install update-manager-core -y
 sudo do-release-upgrade -f DistUpgradeViewNonInteractive
 
+# Install zsh and set as default
 echo "Install and switch default shell to zsh..."
 sudo apt update -y && sudo apt install -y zsh
 sudo usermod -s $(which zsh) $(whoami)
@@ -16,38 +17,50 @@ mkdir -p ~/.config/nvim
 mkdir -p ~/.config/starship
 mkdir -p ~/.config/wezterm
 
+# Copy in core configs
 echo "Copying in configs..."
 cp -r ~/dotfiles/.config/nvim/* ~/.config/nvim/
 cp -r ~/dotfiles/.config/starship/* ~/.config/starship/
 cp -r ~/dotfiles/.config/wezterm/* ~/.config/wezterm/
 cp ~/dotfiles/.zshrc ~/.zshrc
 
+# Install system dependencies
+echo "Installing dependencies..."
+sudo apt update -y && sudo apt install -y build-essential gcc gh make ripgrep tree unzip xclip ninja-build gettext cmake
+
+# Install fzf
+echo "Installing fzf..."
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Install zoxide
+echo "Installing zoxide..."
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+# Install nvm + node
 echo "Installing nvm..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-nvm install 22.17.0
-
-echo "Installing dependencies..."
-sudo apt update -y && sudo apt install -y build-essential fzf gcc gh make ripgrep tree unzip xclip zoxide
+nvm install node
 
 # Install pyenv
 echo "Installing pyenv..."
 curl -fsSL https://pyenv.run | sh
 
+# Install pre-built lazygit
 echo "Installing lazygit..."
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
 
+# Install starship from installer
+echo "Installing starship..."
+curl -sS https://starship.rs/install.sh | sh -s -- --yes
+
+# Install neovim by building from source
 echo "Installing neovim..."
-# Install pre-built neovim
-sudo apt-get install -y ninja-build gettext cmake
 git clone https://github.com/neovim/neovim.git
 cd neovim
 git checkout v0.11.2
 make CMAKE_BUILD_TYPE=Release
 sudo make install
-
-echo "Installing starship..."
-# Install starship from installer
-curl -sS https://starship.rs/install.sh | sh -s -- --yes
