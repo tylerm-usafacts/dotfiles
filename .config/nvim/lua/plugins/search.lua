6282687a3163d7c3b44a1516833b3b869a90d82e
tylerm <tylerm@usafacts.org>
2026-03-06 12:08:51 -0800

    feat: add grug-far + remove containers.lua

--- .config/nvim/lua/plugins/search.lua
+++ .config/nvim/lua/plugins/search.lua
@@ -83,4 +83,21 @@ return {
   {
     'nvim-telescope/telescope-symbols.nvim',
   },
+
+  {
+    'MagicDuck/grug-far.nvim',
+    -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+    -- additional lazy config to defer loading is not really needed...
+    config = function()
+      -- optional setup call to override plugin options
+      -- alternatively you can set options with vim.g.grug_far = { ... }
+      require('grug-far').setup {
+        -- options, see Configuration section below
+        -- there are no required options atm
+      }
+      vim.keymap.set({ 'n', 'x' }, '<leader>si', function()
+        require('grug-far').open { visualSelectionUsage = 'operate-within-range' }
+      end, { desc = 'grug-far: Search within range' })
+    end,
+  },
 }
