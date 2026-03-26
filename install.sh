c153dc069fcf6d4241b23390941b425c14a51926
tylerm <tylerm@usafacts.org>
2026-03-26 13:41:20 -0700

    fix: fzf idempotency installation issue

--- install.sh
+++ install.sh
@@ -54,7 +54,11 @@ install_macos() {
 install_linux_fzf() {
     command -v fzf &>/dev/null && return
     echo "Installing fzf..."
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+
+    if [[ ! -d "$HOME/.fzf" ]]; then
+        git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
+    fi
+
     ~/.fzf/install --all
 }
 
